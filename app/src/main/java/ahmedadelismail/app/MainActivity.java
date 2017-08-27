8cd85b4777c983f19ec8f70538f220c1c8b8c6b2
Ahmed Adel Ismail <ahm3d.ad3l@gmail.com>
2017-08-27 23:57:42 +0200

    updated the code in the sample Application

--- app/src/main/java/ahmedadelismail/app/MainActivity.java
+++ app/src/main/java/ahmedadelismail/app/MainActivity.java
@@ -1,16 +1,65 @@
 package ahmedadelismail.app;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    private final CompositeDisposable disposables = new CompositeDisposable();
     private final MainViewModel viewModel = new MainViewModel();
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text_view);
+
+        disposables.add(viewModel.textViewLabel.asObservable().subscribe(updateTextView()));
+        disposables.add(viewModel.toastMessage.asObservable().subscribe(showToast()));
+
+    }
+
+
+    @NonNull
+    private Consumer<String> updateTextView() {
+        return new Consumer<String>()
+        {
+            @Override
+            public void accept(String s) throws Exception {
+                textView.setText(s);
+            }
+        };
+    }
+
+    private Consumer<? super String> showToast() {
+        return new Consumer<String>()
+        {
+            @Override
+            public void accept(String s) throws Exception {
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    protected void onResume() {
+        super.onResume();
+        viewModel.textViewLabel.set(viewModel.randomLabel());
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        disposables.clear();
+        viewModel.clear();
+        textView = null;
+        super.onDestroy();
     }
 }
--- app/src/main/java/ahmedadelismail/app/MainViewModel.java
+++ app/src/main/java/ahmedadelismail/app/MainViewModel.java
@@ -1,15 +1,26 @@
 package ahmedadelismail.app;
 
+import io.reactivex.properties.Consumable;
+import io.reactivex.properties.Property;
+
 /**
  * Created by Ahmed Adel Ismail on 8/27/2017.
  */
-
 class MainViewModel
 {
 
+    final Property<String> textViewLabel = new Property<>();
+    final Consumable<String> toastMessage = new Consumable<>("started");
 
-    void onDestroy(){
+    final String randomLabel() {
+        return (int)(Math.random() * 10) % 2 == 0
+                ? "Even number label"
+                : "Odd number label";
+    }
 
+    void clear() {
+        textViewLabel.clear();
+        toastMessage.clear();
     }
 
 }
